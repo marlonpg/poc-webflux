bedd708af3d5a9f1be2a854a6e4af252121fba1f
Marlon Gamba <marlonpgamba@gmail.com>
2025-04-22 01:27:02 -0300

    Webflux ConcatMap and FlatMap.

--- src/main/java/com/gambasoftware/poc/webflux/service/FluxAndMonoService.java
+++ src/main/java/com/gambasoftware/poc/webflux/service/FluxAndMonoService.java
@@ -2,11 +2,34 @@ package com.gambasoftware.poc.webflux.service;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
 
 public class FluxAndMonoService {
 
     public Flux<String> getNames(){
         return Flux.fromIterable(List.of("migi", "nami", "tini"));
+    }
+
+    public Flux<String> getNamesDelayed(){
+        return Flux.fromIterable(List.of("migi", "nami", "tini"))
+                .log()
+                .delayElements(Duration.ofMillis(1000));
+    }
+
+    //Flatmap is concurrent
+    public Flux<String> getNamesFlatMap(){
+        return Flux.fromIterable(List.of("migi", "nami", "tini"))
+                .flatMap(s -> Flux.fromArray(s.split(""))
+                        .delayElements(Duration.ofMillis(100)))
+                .log();
+    }
+
+    //Concat preserve the order but it is sequential so it will take more time
+    public Flux<String> getNamesConcat(){
+        return Flux.fromIterable(List.of("migi", "nami", "tini"))
+                .concatMap(s -> Flux.fromArray(s.split(""))
+                        .delayElements(Duration.ofMillis(100)))
+                .log();
     }
 }
--- src/test/java/com/gambasoftware/poc/webflux/service/FluxAndMonoTest.java
+++ src/test/java/com/gambasoftware/poc/webflux/service/FluxAndMonoTest.java
@@ -13,23 +13,39 @@ public class FluxAndMonoTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(FluxAndMonoTest.class);
 
     @Test
+    public void testFlatMap(){
+        Flux.fromIterable(List.of("migi", "nami", "tini"))
+                .flatMap(s -> Flux.fromArray(s.split("")))
+                .log()
+                .subscribe(LOGGER::info);
+    }
+
+    @Test
+    public void testMap(){
+        FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
+        var namesFlux = fluxAndMonoService.getNames();
+
+        StepVerifier.create(namesFlux.map(String::toUpperCase))
+                .expectNext("MIGI", "NAMI", "TINI")
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testFilter(){
+        StepVerifier.create(Flux.fromIterable(List.of("mimao", "naminha", "tini"))
+                .filter(s -> s.length() < 5))
+                .expectNext("tini")
+                .verifyComplete();
+    }
+
+    @Test
     public void testGetNamesFlux(){
         FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
         var namesFlux = fluxAndMonoService.getNames();
 
         StepVerifier.create(namesFlux)
                 .expectNext("migi", "nami", "tini")
-                .expectNextCount(0)
-                .verifyComplete();
-    }
-
-    @Test
-    public void testMapForGetNamesFlux(){
-        FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
-        var namesFlux = fluxAndMonoService.getNames();
-
-        StepVerifier.create(namesFlux.map(String::toUpperCase))
-                .expectNext("MIGI", "NAMI", "TINI")
                 .expectNextCount(0)
                 .verifyComplete();
     }
@@ -45,5 +61,32 @@ public class FluxAndMonoTest {
     public void monoTest(){
         Mono.just("jatobi")
                 .subscribe(LOGGER::info);
+    }
+
+    @Test
+    public void testDelay(){
+        FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
+
+        StepVerifier.create(fluxAndMonoService.getNamesDelayed())
+                .expectNext("migi", "nami", "tini")
+                .verifyComplete();
+    }
+
+    @Test
+    void getNamesFlatMap() {
+        FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
+        StepVerifier.create(fluxAndMonoService.getNamesFlatMap())
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    @Test
+    void getNamesConcat() {
+        FluxAndMonoService fluxAndMonoService = new FluxAndMonoService();
+        StepVerifier.create(fluxAndMonoService.getNamesConcat())
+                .expectNextCount(12)
+                .verifyComplete();
+
     }
 }
